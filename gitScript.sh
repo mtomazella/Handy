be2763f5ddfdf701e29bf58c1c32e944ca1f7630
MatheusTomazella <matheustomazella250504@gmail.com>
2020-07-25 13:43:23 -0300

    2020-07-25

--- gitScript.sh
+++ gitScript.sh
@@ -5,7 +5,7 @@ data=$(date +%Y-%m-%d)
 	echo -e "\nAdicionando arquivos...\n"
 	git add *
 	echo -e "\nFazendo commit...\n"
-	git commit -m $data
+	git commit -a -m $data
 	echo -e "\n\nFazendo push...\n\n"
 	git push origin master
 	echo -e "\n\nPronto!\n\n"
